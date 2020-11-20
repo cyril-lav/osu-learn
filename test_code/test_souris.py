7e643efa91da7394b3ccad172b1f7b169e8ac141
Sebastien-WAGNER-63 <sebastien.wagner.6320@gmail.com>
2020-11-20 14:00:24 +0100

    test souris

---
+++ test_code/test_souris.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from pynput.mouse import Button, Controller 
+import random
+import time
+"""
+Created on Fri Nov 20 13:09:30 2020
+
+@author: sebas
+"""
+mouse = Controller()
+x = random.randint(0,1500)
+y = random.randint(0,1500)
+mouse.position = (x, y)
