8f3e97cdd5fdbaa1062c52ebd5152757ea235282
Sebastien-WAGNER-63 <sebastien.wagner.6320@gmail.com>
2020-11-20 14:35:57 +0100

    modif test souris

--- test_code/test_souris.py
+++ test_code/test_souris.py
@@ -8,6 +8,11 @@ Created on Fri Nov 20 13:09:30 2020
 @author: sebas
 """
 mouse = Controller()
+mouse.click (Button.right, 1)
+
+
+"""
 x = random.randint(0,1500)
 y = random.randint(0,1500)
 mouse.position = (x, y)
+"""
