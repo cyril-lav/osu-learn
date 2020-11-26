d52c20ffe90aa2fd104dfbd970a0236687d1eb2f
Justin Mieran <unconfigured@null.spigotmc.org>
2020-11-26 17:12:39 +0100

    structure des fichier de code

--- test_code/testFenetreOsu.py
+++ test_code/testFenetreOsu.py
@@ -1,13 +1,13 @@
 import win32gui
-#results = []
-#win32gui.EnumWindows(lambda handle, liste: liste.append(handle), results)
-#get = win32gui.GetWindowText
-#for handle in results:
-#    print(get(handle))
+# results = []
+# win32gui.EnumWindows(lambda handle, liste: liste.append(handle), results)
+# get = win32gui.GetWindowText
+# for handle in results:
+#     print(get(handle))
 while(1):
-    window_handle = FindWindow(None, "osu!")
+    window_handle = win32gui.FindWindow(None, "osu!")
     if window_handle!=0 :
-        window_rect   = GetWindowRect(window_handle)
+        window_rect = win32gui.GetWindowRect(window_handle)
         print(window_rect)
     else :
         print("OSU est fermé")
