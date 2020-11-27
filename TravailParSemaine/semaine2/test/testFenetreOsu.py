ad95f88ec8381254ec305eed04293593bf0fd664
Sebastien-WAGNER-63 <sebastien.wagner.6320@gmail.com>
2020-11-27 15:59:14 +0100

    fixe détéction fenêtre

--- TravailParSemaine/semaine2/test/testFenetreOsu.py
+++ TravailParSemaine/semaine2/test/testFenetreOsu.py
@@ -1,12 +1,18 @@
 import win32gui
-# results = []
-# win32gui.EnumWindows(lambda handle, liste: liste.append(handle), results)
-# get = win32gui.GetWindowText
-# for handle in results:
-#     print(get(handle))
+"""
+results = []
+win32gui.EnumWindows(lambda handle, liste: liste.append(handle), results)
+get = win32gui.GetWindowText 
+if win32gui.GetClassName
+get2 = win32gui.GetClassName
+for handle in results:
+    if get(handle) == "osu!":
+        print(get(handle))
+        print(get2(handle))
+"""
 while(1):
-    window_handle = win32gui.FindWindow("WindowsForms10.Window.2b.app.0.358a177_r10_ad1", "osu!")
-    if window_handle!=0 :
+    window_handle = win32gui.FindWindow(None, "osu!")
+    if window_handle!=0 and win32gui.GetClassName(window_handle)[0:28] == "WindowsForms10.Window.2b.app":
         window_rect = win32gui.GetWindowRect(window_handle)
         print(window_rect)
     else :
